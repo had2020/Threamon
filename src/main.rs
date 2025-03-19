91a5ec9d4285ba2f380876c62bb68118374a77dc
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-03-19 14:33:37 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ struct Creature {
     emoji: String,
     health: f32,
     xp: f32,
-    defense: f32,
     attack: f32,
     special: f32,
-    energy: f32,
+    power: f32,
 }
 
 struct Item {
@@ -38,26 +37,27 @@ fn main() {
             emoji: "👞".to_string(),
             health: 10.0,
             xp: 0.0,
-            defense: 5.0,
             attack: 1.0,
-            special: 5.0,
-            energy: 20.0,
+            special: 2.0,
+            power: 20.0,
         },
         Creature {
             name: "Troy".to_string(),
             emoji: "🧞‍♂️".to_string(),
-            health: 1.0,
+            health: 5.0,
             xp: 0.0,
-            defense: 1.0,
-            attack: 1.0,
+            attack: 3.0,
+            special: 8.0,
+            power: 20.0,
         },
         Creature {
             name: "Dalius".to_string(),
             emoji: "🐮".to_string(),
-            health: 1.0,
+            health: 15.0,
             xp: 0.0,
-            defense: 1.0,
-            attack: 1.0,
+            attack: 2.0,
+            special: 4.0,
+            power: 10.0,
         },
         ]
 
@@ -87,9 +87,13 @@ fn main() {
             playing = true;
         }
         if playing && !has_starter {
-            line(Position { x: 0, y: 0 }, "👞 Tikashoe", "red");
-            line(Position { x: 1, y: 0 }, "🧞‍♂️ Troy", "blue");
-            line(Position { x: 2, y: 0 }, "🐮 Dalius", "yellow");
+            line(Position { x: 0, y: 0 }, "Choose your pal!", "white");
+            line(Position { x: 1, y: 0 }, "👞 Tikashoe", "red");
+            line(Position { x: 1, y: 20 }, "Enjoy balance? ⚖️ (j)", "red");
+            line(Position { x: 2, y: 0 }, "🧞‍♂️ Troy", "blue");
+            line(Position { x: 2, y: 20 }, "Do you prefer war? 🔫 (k)", "blue");
+            line(Position { x: 3, y: 0 }, "🐮 Dalius", "yellow");
+            line(Position { x: 3, y: 20 }, "Are you a coward? (l)", "yellow");
         }
     }
 
