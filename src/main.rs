9b1e7ec8b3604a31fb85f79d2a809204d51321e6
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-03-19 13:06:41 -0700

    updates

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+struct Creature {
+    name: String,
+    health: f32,
+    xp: f32,
+    defense: f32,
+    attack: f32,
+}
+
+struct Player_data {
+    name: String,
+    creatures: Vec<Creature>,
+    xp: f32,
+    //healing_pods:
+}
+
+fn main() {}
