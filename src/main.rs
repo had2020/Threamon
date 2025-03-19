df1173d4dfb705adf2f90670b1d4fcbf79d9e0c4
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-03-19 13:33:26 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use TerimalRtdm::*;
+
 struct Creature {
     name: String,
     health: f32,
@@ -6,11 +8,49 @@ struct Creature {
     attack: f32,
 }
 
+struct Item {
+    id: f32,
+    name: String,
+    health_benefit: f32,
+    cost: f32,
+}
+
 struct Player_data {
     name: String,
     creatures: Vec<Creature>,
     xp: f32,
-    //healing_pods:
+    inventory: Vec<Item>,
 }
 
-fn main() {}
+fn print_keybind() {
+    line(Position { x: 0, y: 0 }, "i - 💰 Inventory", "blue");
+    line(Position { x: 0, y: 20 }, "m - 🏃 Move", "red");
+    line(Position { x: 0, y: 32 }, "s - 🛒 Shop", "yellow");
+}
+
+fn main() {
+    clear();
+    let mut app = App::new();
+
+    raw_line("q 👈 to quit 🏃");
+    raw_line("p 👈 to play 🎲");
+
+    raw_mode(true);
+
+    // app loop
+    loop {
+        clear();
+        collect_presses(&mut app);
+
+        if key_press(&app, "q") {
+            clear();
+            break;
+        }
+
+        if key_press(&app, "p") {
+            print_keybind();
+        }
+    }
+
+    raw_mode(false);
+}
